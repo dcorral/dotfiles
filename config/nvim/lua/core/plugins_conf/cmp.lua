c2f8789d643c571f74c6d2ce181aab7381b640c8
dcorral <hi@dcorral.com>
2023-06-26 20:01:52 +0200

    Clean cmp.lua

--- config/nvim/lua/core/plugins_conf/cmp.lua
+++ config/nvim/lua/core/plugins_conf/cmp.lua
@@ -3,13 +3,6 @@ local luasnip = require('luasnip')
 
 local select_opts = { behavior = cmp.SelectBehavior.Replace }
 
--- local has_words_before = function()
---     local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
---     return col ~= 0 and
---         vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(
---             col, col):match("%s") == nil
--- end
-
 cmp.setup({
 
     snippet = {
@@ -55,8 +48,6 @@ cmp.setup({
                 cmp.select_next_item(select_opts)
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
             else
                 fallback()
             end
@@ -64,20 +55,8 @@ cmp.setup({
         ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item(select_opts)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<C-j>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.scroll_docs(4)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<C-k>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.scroll_docs(-4)
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
             else
                 fallback()
             end
