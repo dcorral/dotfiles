3453aa249eefd3538cb6080aa009b6c2894d3238
dcorral <hi@dcorral.com>
2024-10-09 11:25:50 +0100

    Fix layout

--- utils/toggle_k_layout.sh
+++ utils/toggle_k_layout.sh
@@ -5,11 +5,11 @@ current_layout=$(setxkbmap -query | grep layout | awk '{print $2}')
 if [ "$current_layout" = "us" ]; then
     setxkbmap es
     notify-send -i 'a' -t 3000 "Layout" "🇪🇸"
-    echo "🇪🇸" >/home/dcorral/keyboard_layout
+    echo "es" >/home/dcorral/keyboard_layout
     killall -USR1 i3status
 else
     setxkbmap us
     notify-send -i 'a' -t 3000 "Layout" "🇬🇧"
-    echo "🇬🇧" >/home/dcorral/keyboard_layout
+    echo "gb" >/home/dcorral/keyboard_layout
     killall -USR1 i3status
 fi
